09afee6b60ceee1f159b127afbcc7f3f2c97325b
Raoul Van Hezel <rvanhezel@Raouls-MacBook.local>
2020-04-04 13:19:23 +0200

    people changes

--- src/objects/people.py
+++ src/objects/people.py
@@ -4,6 +4,7 @@ class boy:
 
     def __init__(self, _age):
         self.age = _age
+        self.age_np = np.array([self.age]*10)
 
     def get_age(self):
-        return _age
+        return self.age
